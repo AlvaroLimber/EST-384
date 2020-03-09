c05e5c3115550f61d6495d45c9f7700f6e78a547
Alvaro Limber Chirino Gutierrez <alvaro.rqsbo@gmail.com>
2020-03-09 18:45:31 -0400

    Update clase_9mar.R

--- code/clase_9mar.R
+++ code/clase_9mar.R
@@ -1,19 +1,22 @@
 ################################
-#Taller Componentes Principales
+#Taller Componentes Principales (PCA)
 #Objetivo: Aplicar el PCA
 ################################
 #PASOS
 ################################
 #1. Seleccione una base de datos de interés del repositorio
-#2. Seleccione las variables para el PCA
+#2. Seleccione las variables para el PCA (Según la motivación)
 #3. Calcule el PCA
 #4. Identifique el número de CPs que explican hasta el 90% de la varianza
 #5. Identique las variables correlacionadas con la cantidad de 
 #componentes fuera del 90% del paso anterior
 #6. Calcule nuevamente el componente principal eliminando las variables rebundantes
-#7. Deterine la contidad de componentes principlaes a retener
+#7. Determine la cantidad de componentes principales a retener ()
 #8. Defina un indicador a partir de estos
 #9. Modele un modelo lineal empleando los CPs retenidos.
-#10. Comente los resultados
+#10. Existe algún valor atípico en la base de datos
+#11. Comente los resultados
 
-
+load(url("https://github.com/AlvaroLimber/EST-384/blob/master/data/oct20.RData?raw=true"))
+load(url("https://github.com/AlvaroLimber/EST-384/blob/master/data/endsa.RData?raw=true"))
+load(url("https://github.com/AlvaroLimber/EST-384/blob/master/data/nna16.RData?raw=true"))
